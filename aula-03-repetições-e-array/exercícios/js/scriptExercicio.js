28766ae051ed423c4d2bb4d8e0fd179d9bccca13
nathaliabrazk <111463766+nathaliabrazk@users.noreply.github.com>
2023-11-23 17:40:20 -0300

    Exercício arrays e repetições finalizado

--- aula-03-repetições-e-array/exercícios/js/scriptExercicio.js
+++ aula-03-repetições-e-array/exercícios/js/scriptExercicio.js
@@ -1,8 +1,22 @@
 //1 - Faça uma listagem de 6 filmes de sua escolha com array e faça eles serem impressos no console.log através de repetição
+console.log("FILMES")
 var Filmes = ["Crepúsculo", "JogosVorazes", "Divergente", "A viagem-de-Chihiro", "Meu-amigo-Tototro", "Convergente"];
+Filmes.forEach((cadaFilme) => {
+    console.log(cadaFilme);
+})
 
-var contador = 1;
-do{
-    console.log(Filmes[contador]);
-    contador = contador++;
-}while(contador <= 6);
+//2 - Fazer a tabuada de 8 com repetições
+console.log("TÁBUADA DO 8")
+var  i = 0;
+while(i <= 8){
+    console.log("8 * "+i+ " = "+8 * i);
+    i = i + 1;
+}
+
+//3 - Fazer as tabuadas de 1 até 10 com repetições
+for (let i = 1; i <= 10; ++i) {
+    console.log("TABUADA DO "+i);
+    for (let j = 1; j <= 10; ++j) {
+        console.log(i + " x " + j + " = " + (i * j));
+    }
+}
