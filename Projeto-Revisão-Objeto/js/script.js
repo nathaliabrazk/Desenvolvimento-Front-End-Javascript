158290f21ff64ce83873482ca5be765f9d6b8076
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-11-28 23:16:34 -0300

    Início atividade revisão

--- Projeto-Revisão-Objeto/js/script.js
+++ Projeto-Revisão-Objeto/js/script.js
@@ -1,3 +1,30 @@
 /*faça uma página de cadastramento de filmes onde o usuario possa salvar um filme 
 e suas informações como nome, descrição,data, diretor e categoria e liste ela de a
 lguma forma em algum elemento HTML!*/
+
+//CLASSE
+class Filme {
+    constructor(valTitulo,valDescricao, valDiretor, valLancamento, valCategoria){
+        this.titulo = valTitulo;
+        this.descricao = valDescricao;
+        this.diretor = valDiretor;
+        this.lancamento = valLancamento;
+        this.categoria = valCategoria;
+       }
+}
+
+//FUNÇÃO
+function cadastramento(){
+    let tituloInput = document.querySelector("#titulo").value;
+    let descricaoInput = document.querySelector("#descricao").value;
+    let diretorInput = document.querySelector("#diretor").value;
+    let lancamentoInput = document.querySelector("#lancamento").value;
+    let categoriaInput = document.querySelector("#categoria").value;
+
+    var filme = new Filme
+    resultado.innerHTML = `<div class="carroResultado">
+    <h3>${carro.modelo}</h3>
+    <p>${carro.marca}</p>
+    <span style="color : ${carro.cor};">COR ESCOLHIDA</span>
+    </div>`;
+}
--- aula-05-ObjetoJs/exercícios/js/script.js
+++ aula-05-ObjetoJs/exercícios/js/script.js
@@ -1,53 +1,43 @@
-/*Crie um objeto chamado "carro" que tenha as seguintes propriedades:
-marca: string
-modelo: string
-ano: número
-cor: string
-velocidadeMaxima: número
-velocidadeAtual: número (inicialmente 0)
-Em seguida, crie um método chamado "acelerar" que receba um número como
-argumento e aumente a velocidade atual do carro por esse número.
-Certifique-se de que a velocidade atual não ultrapasse a velocidade
-máxima do carro.*/
-let carro = {
-    marca: "Exemplo",
-    modelo: "Modelo1",
-    ano: 2023,
-    cor: "Azul",
-    velocidadeMaxima: 200,
-    velocidadeAtual: 0,
-  
-    //Método
-    acelerar: function(velocidade) {
-      if (this.velocidadeAtual + velocidade <= this.velocidadeMaxima) {
-        this.velocidadeAtual += velocidade;
-        console.log(`Acelerando. Velocidade Atual: ${this.velocidadeAtual} km/h`);
-      }else {
-        console.log(`Velocidade máxima atingida (${this.velocidadeMaxima} km/h). Não é possível acelerar mais.`);
-      }if(this.velocidadeAtual < 0){
-        console.log("Velocidade inválida! Digite valores acima de 0");
-      }
-    }
-  };
-  console.log("Detalhes do carro:", carro);
-  //utilizando o método
-  carro.acelerar(50); 
-
-  //HTML
-  var btnCarro = document.getElementById("btnCarro");
-  var resultado = document.getElementById("resultado");
-  btnCarro.addEventListener('click', criarCarro);
-
-  function criarCarro(){
-      let marcaInput = document.querySelector("#marca").value;
-      let modeloInput = document.querySelector("#modelo").value;
-      let anoInput = document.querySelector("#ano").value;
-      let corInput = document.querySelector("#cor").value;
-      let velocidadeMaxInput = document.querySelector("#velocidadeMax").value;
-
-      var carro = new carro(marcaInput, modeloInput, anoInput, corInput, velocidadeMaxInput);
-
-      resultado.innerHTML = `<div class="carroResultado">
-      <h3>${carro.modelo}</h3>
-      </div>`;
+class Carro {
+  constructor(valMarca,valModelo, valAno, valCor, valVelocidadeMax){
+   this.marca = valMarca;
+   this.modelo = valModelo;
+   this.ano = valAno;
+   this.cor = valCor;
+   this.velocidadeMax = valVelocidadeMax;
+   this.velocidadeAtual = 0;
   }
+  acelerar(velAcelerada){
+   if(velAcelerada <= 0){
+       return "Operação não permitida! Acelere acima de 0!";
+   }else if(velAcelerada + this.velocidadeAtual > this.velocidadeMax ){
+       return `Operação não permitida! Velocidade máxima atingida!`;
+   }else{
+       this.velocidadeAtual += velAcelerada;
+       return `Velocidade atual é de: ${this.velocidadeAtual}`;
+   }
+  }
+}
+
+
+// HTML
+var btnCarro = document.getElementById("btnCarro");
+var resultado = document.getElementById("resultado");
+
+btnCarro.addEventListener('click', criarCarro);
+
+function criarCarro(){
+   let marcaInput = document.querySelector("#marca").value;
+   let modeloInput = document.querySelector("#modelo").value;
+   let anoInput = document.querySelector("#ano").value;
+   let corInput = document.querySelector("#cor").value;
+   let velocidadeMaxInput = document.querySelector("#velocidadeMax").value;
+
+   var carro = new Carro(marcaInput,modeloInput,anoInput,corInput,velocidadeMaxInput);
+   console.log(carro.cor)
+   resultado.innerHTML = `<div class="carroResultado">
+   <h3>${carro.modelo}</h3>
+   <p>${carro.marca}</p>
+   <span style="color : ${carro.cor};">COR ESCOLHIDA</span>
+   </div>`;
+} 
