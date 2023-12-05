c17dadb22fb662f755667f8e616f4cde44e319b4
nathaliabrazk <111463766+nathaliabrazk@users.noreply.github.com>
2023-12-05 17:00:34 -0300

    Aula 10 jquery animation

--- aula-10-Jquery-Animation/materialAula/js/script.js
+++ aula-10-Jquery-Animation/materialAula/js/script.js
@@ -8,6 +8,36 @@ $("#animacao2").on('click', function(){
     caixa.animate({width : "300px", height : "300px", borderRadius : "0%"}).
     animate({marginLeft : "300px", rotate : "360deg"});
 });
+
 $("#animacao3").on('click', function(){
-    caixa.animate({margin : "0px", rotate : "-360deg"});
+    caixa.animate({margin : "0px", rotate : "-360deg"},{duration : 3000});
+});
+
+$("#animacao4").on('click', function(){
+    caixa.animate({margin : "100px",width : "600px", height : "600px"},{complete : () => {alert("me contrata ;)")}});
+});
+
+$("#animacao5").on('click', function(){
+    //caixa.hide(500);
+    caixa.slideUp(500);;
+    //caixa.fadeOut(500);
+});
+
+$("#animacao6").on('click', function(){
+    //caixa.show(500);
+    //caixa.slideDown(500);
+    caixa.fadeIn(500);
+});
+
+$("#animacao7").on('click', function(){
+    //caixa.fadeToggle(500);
+    caixa.slideToggle(500);
+});
+
+$("#animacao8").on('click', function(){
+    caixa.css("transition","1s");
+    caixa.css("backgroundColor","red");
+    $("img").attr("src", "https://images.pexels.com/photos/551628/pexels-photo-551628.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+    $("img").attr("width", "300px");
+    $("#teste").attr("placeholder","Mensagem Legal!")
 });
